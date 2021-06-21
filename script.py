bdcbb02de6824ed0594c1f4560c2f127e49226e0
Forkpersonal <86183283+Forkpersonal@users.noreply.github.com>
2021-06-21 06:57:16 +0530

    class script(object):       START_MSG = """ <b>Hi {}  You can call this as an Aliya Bot by @telgram_movies if you like :D  Bot simply search for the files from provided channel according to given query and gives link to those files as buttons!  So no need to wandering for your desired movies from now on!  For more click <i>help</i></b>"""       HELP_MSG = """<b>How to use the bot??</b> <i>     * This is strictly made for @telegrm_movies   * so go and type the movie name with correct spelling @telgram_movies   * Bot will work now :)  Search with correct spelling  Bot will automatically search for your file and give links to that! </i>  <b>© @telegrm_movies</b>"""       ABOUT_MSG = """⭕️<b>My Name : Aliya Bot</b>      ⭕️<b>Creater :</b> @Shon_G  ⭕️<b>Language :</b> <code>Python3</code>  ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>   """

--- script.py
+++ script.py
@@ -3,11 +3,11 @@ class script(object):
 
     START_MSG = """ <b>Hi {}
 
-You can call this as an Auto Filter Bot by @telgram_movies if you like :D
+You can call this as an Aliya Bot by @telgram_movies if you like :D
 
 Bot simply search for the files from provided channel according to given query and gives link to those files as buttons!
 
-So no need to add filters for your files or movies from now on!
+So no need to wandering for your desired movies from now on!
 
 For more click <i>help</i></b>"""
 
@@ -20,14 +20,14 @@ For more click <i>help</i></b>"""
 
 * Bot will work now :)
 
-No need add each filter again!
+Search with correct spelling 
 Bot will automatically search for your file and give links to that!
 </i>
 
 <b>© @telegrm_movies</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Aliya Bot</b>
     
 ⭕️<b>Creater :</b> @Shon_G
 
